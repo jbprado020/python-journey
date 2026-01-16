862c2334dc36c3b05bb71ee528e7781dc83e59de
Julianne Benedict Prado <jbprado013@gmail.com>
2026-01-16 13:23:56 +0800

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,17 @@
+# January 16, 2026
+# Today I am learning about operators in Python.
+
+# Operators are used to perform operations on variables and values.
+
+print(10 + 5)      # Addition
+print(10 - 5)      # Subtraction
+print(10 * 5)      # Multiplication
+print(10 / 5)      # Division
+print(10 % 3)      # Modulus
+print(10 ** 2)     # Exponentiation
+print(10 // 3)     # Floor Division
+
+sum1 = 100 + 50
+sum2 = sum1 + 250
+sum3 = sum2 + sum2
+
